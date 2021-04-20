989b0acf6f5019b671a381b249ced1a643002077
SimonGideon <simongideon918@gmail.com>
2021-04-20 09:16:14 +0300

    Rock Paper Scissor

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,32 @@
+# Rock paper Scissor
+from random import randint
+
+# moves for the player
+moves = ["rock", "paper", "scissors"]
+
+while True:
+    computer = moves[randint(0,2)]
+    player = input("rock, paper or scissor ? (or end the game) ").lower()
+    if player == "end the game":
+        print("The game has ended.")
+        break
+    elif player == computer:
+        print("Tie")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "beats", player)
+        else:
+            print("You win!", player, "beats", computer)
+    elif player == "paper":
+        if computer == "scissors":
+            print("You  lose !", computer, "beats", player)
+        else:
+            print("You win", player, "beats", computer)
+    elif player == "scissors":
+        if computer == "rock":
+            print("You  lose !", computer, "beats", player)
+        else:
+            print("You win", player, "beats", computer)
+    else:
+        print("Check your spelling.....")
+
