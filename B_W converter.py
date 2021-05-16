b2036d95715011144d4b1cd583f398d1ed0783b8
SimonGideon <simongideon918.com>
2021-05-16 23:46:20 +0300

    B_W photo converter

---
+++ B_W converter.py
@@ -0,0 +1,9 @@
+import pip
+import PIL
+
+from PIL import Image
+
+img= Image.open("D:\PICTURES\hahaha.png")
+blackAndWhite = img.convert("L")
+blackAndWhite.save('D:\PICTURES\Bw_hahaha.png')
+blackAndWhite.show()
