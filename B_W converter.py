08e0e2fdb611feee026628dc8b695b9572a9154a
SimonGideon <simongideon918@gmail.com>
2021-08-28 00:24:00 +0300

    Daily dose

--- B_W converter.py
+++ B_W converter.py
@@ -1,9 +1,16 @@
-wqeq3import pip
 import PIL
-
+import winsound
 from PIL import Image
 
-img= Image.open("D:\PICTURES\hahaha.png")
+img = Image.open("D:\PICTURES\PO74763662.jpg")
+
 blackAndWhite = img.convert("L")
 blackAndWhite.save('D:\PICTURES\Bw_hahaha.png')
 blackAndWhite.show()
+
+if blackAndWhite:
+    freq = 2500
+    dur = 1000
+    winsound.Beep(freq, dur)
+else:
+    print("gfgg")
--- Pie chart in py.py
+++ Pie chart in py.py
@@ -1,6 +1,8 @@
 
 import matplotlib.pyplot as a
-p=[29, 21, 17, 13, 20]
+x = input("Enter Value: ")
+x = int(x)
+p=[x, 21, 17, 13, 10]
 pl=["Mark","Jayson","Victor","Loice","Maureen"]
 a.pie(p, labels=pl)
 a.show()
