bf9a54329bfdeb13bc1da951cdc8b85929f817f7
SimonGideon <simongideon918@gmail.com>
2021-08-31 23:04:56 +0300

    Initial Commit

--- Django_3/source/Polls/admin.py
+++ Django_3/source/Polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question
+from .views import ContactFormView
+admin.site.register()
 admin.site.register(Question)
-
+admin.site.register(ContactFormView)
 
 # Register your models here.
