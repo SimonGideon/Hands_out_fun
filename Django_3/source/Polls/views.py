076719ce10190b8d14227f7ecc5a6c52e3f25327
SimonGideon <simongideon918@gmail.com>
2021-09-02 22:45:10 +0300

    auth in views

--- Django_3/source/Polls/views.py
+++ Django_3/source/Polls/views.py
@@ -1,13 +1,26 @@
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import NameForm
 from django.views.generic import TemplateView
+from django.contrib.auth import update_session_auth_hash
 
+
+def password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+    else:
+        pass
 class HomeView(TemplateView):
     template_name = '.templates/forms.html'
+
     def get_context_data(self, **kwargs):
         kwargs['Enviroment'] = 'Prodution'
         return super().get_context_data(**kwargs)
+
 
 # Create your views here.
 def get_name(request):
